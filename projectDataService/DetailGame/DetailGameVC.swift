3a13501acb6c8fa2fac72ab83340edb0f7541187
TRAING Serey <t.sereypich@gmail.com>
2018-09-04 17:12:57 +0200

    add screens detail game, update profile

---
+++ projectDataService/DetailGame/DetailGameVC.swift
@@ -0,0 +1,34 @@
+//
+//  DetailGameVC.swift
+//  projectDataService
+//
+//  Created by TRAING Serey on 04/09/2018.
+//  Copyright © 2018 AlkRox. All rights reserved.
+//
+
+import UIKit
+
+class DetailGameVC: DefaultVC {
+
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var editorLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var askLoanButton: UIButton!
+    @IBOutlet weak var addListButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func showListUser(_ sender: Any) {
+    }
+    
+    @IBAction func addListClicked(_ sender: Any) {
+    }
+}
---
+++ projectDataService/UpdateProfile/UpdateProfileVC.swift
@@ -0,0 +1,27 @@
+//
+//  UpdateProfileVC.swift
+//  projectDataService
+//
+//  Created by TRAING Serey on 04/09/2018.
+//  Copyright © 2018 AlkRox. All rights reserved.
+//
+
+import UIKit
+
+class UpdateProfileVC: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func submitClicked(_ sender: Any) {
+    }
+}
